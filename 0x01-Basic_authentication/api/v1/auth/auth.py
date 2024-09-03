6092f4cc19b2784c30451f86e2d7130d5216040e
Method Mkoma <gershommethod02@gmail.com>
2024-09-03 13:30:56 +0300

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" The module containing auth class
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    ''' Class itself
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        ''' authenticating method
+        '''
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        ''' authenticating header
+        '''
+        return request
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' current user
+        '''
+        return request
