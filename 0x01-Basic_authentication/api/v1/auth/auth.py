fb241ff68aab6237f9f853821db33e5d04b432f5
Method Mkoma <gershommethod02@gmail.com>
2024-09-04 14:02:29 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' authenticating method
         '''
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path and excluded_paths:
+            if path in excluded_paths or path + '/' in excluded_paths:
+                return False
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         ''' authenticating header
